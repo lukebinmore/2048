f46c46858263b70a3e87b2b240cc52cd8e88a427
Luke Binmore <lukebinmore@gmail.com>
2021-12-06 13:34:54 +0000

    Update - Game Section Resize Script

---
+++ assets/js/windowscript.js
@@ -0,0 +1,28 @@
+function getVerticalSpace() {
+  let topRec = document.getElementById("score-section").getBoundingClientRect();
+  let bottomRec = document.getElementById("controls-section").getBoundingClientRect();
+
+  return bottomRec.top - topRec.bottom;
+}
+
+function compareSpace() {
+  let vertical = getVerticalSpace();
+  let horizontal = document.body.getBoundingClientRect().right;
+
+  if (vertical <= horizontal) {
+    return vertical;
+  } else {
+    return horizontal;
+  }
+}
+
+function setSpace() {
+  let game = document.getElementById("game-section");
+
+  game.style.width = compareSpace() + "px";
+  game.style.height = compareSpace() + "px";
+}
+
+window.addEventListener("resize", function () {
+  setSpace();
+});
