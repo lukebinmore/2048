bc1f0a960f1ce6a065c006bb1f37ba57c8e90274
Luke Binmore <lukebinmore@gmail.com>
2021-12-07 18:21:30 +0000

    Update - Cookie Manager Functions

--- assets/js/cookiemanager.js
+++ assets/js/cookiemanager.js
@@ -1,9 +1,17 @@
+function createCookie() {
+  document.cookie = "expires=Tue, 19 Jan 2038 04:14:07 GMT";
+}
+
+function setCookie(name, value) {
+  document.cookie = name + "=" + value + ";";
+}
+
 function getCookie(name){
   let cookies = document.cookie.split(";");
 
   for (let cookie of cookies) {
     let cookiePair = cookie.split("=");
-    
+
     if (cookiePair[0].trim() === name) {
       return cookiePair[1];
     }
