ab0e9d3f3fbab3f0d8a216ee5be6ac0d0c61e98d
Luke Binmore <lukebinmore@gmail.com>
2021-12-07 19:52:53 +0000

    Update - Username Popup Section / Intro Section Layout, Styling and Script

--- assets/js/usernamescript.js
+++ assets/js/usernamescript.js
@@ -1,18 +1,69 @@
+function updateUsernameField() {
+  let usernameInput = document.getElementById("input-username");
+  let cookiesInput = document.getElementById("input-cookies");
+
+  if (getCookie("username") !== "") {
+    usernameInput.value = getCookie("username");
+  } else {
+    createCookie();
+  }
+
+  if (getCookie("cookies") == "agreed") {
+    cookiesInput.checked = true;
+  }
+}
+
 function changePage() {
   let pageElements = document.getElementsByClassName("main-page");
-  let usernameSection = document.getElementById("username-section");
+  let introSection = document.getElementById("intro-section");
 
-  usernameSection.hidden = true;
+  introSection.hidden = true;
 
   for (let element of pageElements) {
     element.hidden = false;
   }
 }
 
-function clickPlay() {
-  changePage();
-  startWindowScript();
-  startTipsScript();
+function validateInput() {
+  let usernameInput = document.getElementById("input-username");
+  let cookiesInput = document.getElementById("input-cookies");
+
+  if (usernameInput.value === "") {
+    return false;
+  }
+
+  if (!cookiesInput.checked) {
+    return false;
+  }
+
+  setCookie("username", usernameInput.value);
+  setCookie("cookies", "agreed");
+
+  return true;
 }
 
-console.log(getCookie("username"));
+function resetFields() {
+  document.getElementById("input-username").value = "";
+  document.getElementById("input-cookies").checked = false;
+
+  setCookie("username", "");
+  setCookie("cookies", "");
+}
+
+function clickPlay() {
+  if (validateInput()) {
+    changePage();
+    startWindowScript();
+    startTipsScript();
+  }
+}
+
+updateUsernameField();
+
+document.getElementById("intro-play").addEventListener("click", function () {
+  clickPlay();
+})
+
+document.getElementById("intro-reset").addEventListener("click", function() {
+  resetFields();
+})
