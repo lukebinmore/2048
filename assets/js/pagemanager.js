a18b09c04f0a7a58a1ee9fe4a4f743dd57677dc1
Luke Binmore <lukebinmore@gmail.com>
2021-12-10 23:45:39 +0000

    Change - Page Manager & Window Script Merge

--- assets/js/pagemanager.js
+++ assets/js/pagemanager.js
@@ -83,8 +83,47 @@ function closePage() {
 
 
 
+// Game section resize functions
+
+function getVerticalSpace() {
+  let topRec = document.getElementById("score-section").getBoundingClientRect();
+  let bottomRec = document.getElementById("controls-section").getBoundingClientRect();
+
+  return bottomRec.top - topRec.bottom;
+}
+
+function compareSpace() {
+  let vertical = getVerticalSpace();
+  let horizontal = document.body.getBoundingClientRect().right;
+
+  if (vertical <= horizontal) {
+    return vertical;
+  } else {
+    return horizontal;
+  }
+}
+
+function setSpace() {
+  let game = document.getElementById("game-section");
+
+  game.style.width = compareSpace() + "px";
+  game.style.height = compareSpace() + "px";
+}
+
+function startWindowScript() {
+  setSpace();
+  window.addEventListener("resize", function () {
+    setSpace();
+  });
+}
 
 
+
+
+
+
+
+// Element declerations
 
 let instructionsButton = document.getElementById("instructions-button");
 let historyButton = document.getElementById("history-button");
