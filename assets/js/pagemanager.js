bc69552cffbb069a9663914181edd21c767e827f
Luke Binmore <lukebinmore@gmail.com>
2021-12-10 23:44:11 +0000

    Change - Page Manager & Cookie Manager Merge

--- assets/js/pagemanager.js
+++ assets/js/pagemanager.js
@@ -1,3 +1,31 @@
+// Cookie managment functions
+
+function createCookie() {
+  document.cookie = "expires=Tue, 19 Jan 2038 04:14:07 GMT";
+}
+
+function setCookie(name, value) {
+  document.cookie = name + "=" + value + ";";
+}
+
+function getCookie(name){
+  let cookies = document.cookie.split(";");
+
+  for (let cookie of cookies) {
+    let cookiePair = cookie.split("=");
+
+    if (cookiePair[0].trim() === name) {
+      return cookiePair[1];
+    }
+  }
+
+  return "";
+}
+
+
+
+// User input management functions
+
 function clickInput(e) {
   switch (e.target) {
     case (instructionsButton):
@@ -24,6 +52,13 @@ function clickInput(e) {
   }
 }
 
+
+
+
+
+
+// Menu button functions
+
 function openInstructions() {
   closePage();
   closePageButton.hidden = false;
@@ -44,6 +79,12 @@ function closePage() {
   closePageButton.hidden = true;
   gamePage.hidden = false;
 }
+
+
+
+
+
+
 
 let instructionsButton = document.getElementById("instructions-button");
 let historyButton = document.getElementById("history-button");
