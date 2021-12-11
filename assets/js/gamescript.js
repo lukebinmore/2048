da4444ead2afa9eb99c898aa3c03dd5cd7bd4b6e
Luke Binmore <lukebinmore@gmail.com>
2021-12-11 15:12:54 +0000

    Update - Game Script - Set Tile Size & Get Current Grid

--- assets/js/gamescript.js
+++ assets/js/gamescript.js
@@ -1,26 +1,58 @@
 function createGrid() {
   for (let i = 0; i < gameGridSize * gameGridSize; i++) {
     let tile = document.createElement("div");
+
     tile.innerHTML = 0;
     gameGrid.appendChild(tile);
     tiles.push(tile);
   }
 
+  setTileSize();
+
   createTile();
   createTile();
 }
 
+function setTileSize() {
+  let tileSize = 100 / gameGridSize;
+  for (let div of gameGrid.getElementsByTagName("div")) {
+    div.style.width = tileSize + '%';
+    div.style.height = tileSize + '%';
+  }
+}
+
 function createTile() {
   let randomNum = Math.floor(Math.random() * tiles.length);
-  if (tiles[randomNum] == 0) {
-    tiles[randomNum] = 2;
+
+  if (tiles[randomNum].innerHTML == 0) {
+    tiles[randomNum].innerHTML = 2;
   } else {
     createTile();
   }
 }
 
+function getCurrentGrid() {
+  let currentGrid = [];
+
+  for (let i = 0; i < gameGridSize * gameGridSize; i++) {
+    if (i % gameGridSize === 0) {
+      let totals = [];
+      let row = [];
+
+      for (let i2 = 0; i2 < gameGridSize; i2++) {
+        row.push(parseInt(tiles[i + i2].innerHTML));
+      }
+
+      currentGrid.push(row);
+    }    
+  }
+
+  return currentGrid;
+}
+
 function startGame() {
   createGrid();
+  getCurrentGrid();
 }
 
 // Game elements
